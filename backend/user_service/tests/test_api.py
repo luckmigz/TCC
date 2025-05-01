a0eef22d543c031fa11402dc3bed92d187c3d15d
Lucas M. <lucas.matosnegri@hotmail.com>
2025-05-01 19:39:07 -0300

    feat: adicionado test automatico ao user_service e feito o .gitignore esquecido

---
+++ backend/user_service/tests/test_api.py
@@ -0,0 +1,25 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../app')))
+
+from fastapi.testclient import TestClient
+from app import app  # ou ajuste para o caminho correto
+
+client = TestClient(app)
+
+def test_create_user():
+    response = client.post(
+        "/create", json={"email": "user@example.com", "name": "User Example", "password": "12345"}
+    )
+    assert response.status_code == 201
+    assert response.json()["email"] == "user@example.com"
+
+def test_get_user():
+    response = client.get("/?email=user@example.com")
+    assert response.status_code == 200
+    assert response.json()["email"] == "user@example.com"
+
+def test_get_user_not_found():
+    response = client.get("/?email=nonexistent@example.com")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "User not found"
