5f6eb741b82e1e5dcb27120db4a1e5d99d87ccf7
Lucas M. <lucas.matosnegri@hotmail.com>
2025-05-01 20:57:34 -0300

    fix: pequenas modificações

--- backend/user_service/tests/test_api.py
+++ backend/user_service/tests/test_api.py
@@ -6,20 +6,19 @@ from fastapi.testclient import TestClient
 from app import app  
 
 client = TestClient(app)
-
 def test_create_user():
     response = client.post(
-        "/create", json={"email": "user@example.com", "name": "User Example", "password": "12345"}
+        "/user/create", json={"email": "user@example.com", "name": "User Example", "password": "12345"}
     )
-    assert response.status_code == 201
+    assert response.status_code == 201 or response.status_code == 200
     assert response.json()["email"] == "user@example.com"
 
 def test_get_user():
-    response = client.get("/?email=user@example.com")
+    response = client.get("/user?email=user@example.com")
     assert response.status_code == 200
     assert response.json()["email"] == "user@example.com"
 
 def test_get_user_not_found():
-    response = client.get("/?email=nonexistent@example.com")
+    response = client.get("/user?email=nonexistent@example.com")
     assert response.status_code == 404
     assert response.json()["detail"] == "User not found"
