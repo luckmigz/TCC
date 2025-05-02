a2c0b7fe6d963a6ad655f24efee403a1e0fbb372
Lucas M. <lucas.matosnegri@hotmail.com>
2025-05-02 17:40:37 -0300

    fix: arrumado o erro de criação de restaurante

--- backend/restaurant_service/app/service/restaurant.py
+++ backend/restaurant_service/app/service/restaurant.py
@@ -30,32 +30,41 @@ class RestaurantService:
             return []
 
         with cls.DB_PATH.open("r", encoding="utf-8") as f:
-            data = json.load(f)
-            return [Restaurant(**restaurant) for restaurant in data]
+            try:
+                data = json.load(f)
+                return [Restaurant(**restaurant) for restaurant in data]
+            except json.JSONDecodeError:
+                return []
+        
     @classmethod
     def _save_restaurants(cls,restaurants: List[Restaurant]):
         with cls.DB_PATH.open("w", encoding="utf-8") as f:
             json.dump([restaurant.dict() for restaurant in restaurants], f, ensure_ascii=False, indent=4)
             
     @classmethod
-    async def create(cls,restaurant: Restaurant) -> Restaurant:
-        restaurants = await cls._load_restaurants()
+    async def create(cls,restaurant: dict) -> Restaurant:
+        restaurant = Restaurant(**restaurant)
+        restaurants = cls._load_restaurants()
+        
         max_id = max((r.id for r in restaurants if r.id is not None), default=0)
         restaurant.id = max_id + 1
+        
         restaurant.address = await fetch_cep(restaurant.cep)
         if restaurant.address is None:
             raise ValueError("Invalid CEP or unable to fetch address.")
+        
         restaurants.append(restaurant)
         cls._save_restaurants(restaurants)
+        
         return restaurant
 
     @classmethod
     def get(cls,name: str) -> Optional[Restaurant]:
-        return next((restaurant for restaurant in cls._load_restaurants() if restaurant.id == id), None)
+        return next((restaurant for restaurant in cls._load_restaurants() if restaurant.name == name), None)
 
     @classmethod
     async def update(cls,updated: Restaurant, cnpj:str) -> Optional[Restaurant]:
-        restaurants =  await cls._load_restaurants()
+        restaurants = cls._load_restaurants()
         for i, r in enumerate(restaurants):
             if r.cnpj == cnpj:
                 updated.id = r.id
