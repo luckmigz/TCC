077a7e2bfea818380759fd1a25b491a456729001
Lucas M. <lucas.matosnegri@hotmail.com>
2025-05-02 16:09:07 -0300

    feat: adicionado função de atualizar a ocupação, modificado os __init__.py para poder facilitar o import, modificado o model para conter parametros como cep e cnpj, foi criado tambem a função de buscar cep, além disso algumas funções foram atualizadas para serem async

--- backend/restaurant_service/models/model.py
+++ backend/restaurant_service/models/model.py
@@ -4,7 +4,9 @@ from typing import Optional
 class Restaurant(BaseModel):
     id: Optional[int] 
     name: str 
-    address: str 
+    address: str | None = None
+    cep: str
+    cnpj: str
     phone_number: str 
     cuisine_type: str 
     rating: Optional[float] 
