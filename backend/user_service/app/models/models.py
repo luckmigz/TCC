2bddc6639b6b0d0cbf3822e0a490d3e9a197f5e4
Lucas M. <lucas.matosnegri@hotmail.com>
2025-09-09 19:39:20 -0300

    feat: feito a caraia do sistema de usuario unificado com o de restaurante para bct do aws ;)

--- backend/user_service/app/models/models.py
+++ backend/user_service/app/models/models.py
@@ -1,22 +1,22 @@
 from pydantic import BaseModel, EmailStr
-from typing import Optional
+from typing import Optional, Union
 
 class User(BaseModel):
-    id: Optional[int] = None
+    id: Optional[str] = None
     name: str
     email: EmailStr
     password: str
-    cpf: str  # Campo que pode conter CPF ou CNPJ
+    cpf: str  # Campo que pode conter CPF (pessoa física) ou CNPJ (pessoa jurídica/restaurante)
 
 class Restaurant(BaseModel):
-    id: Optional[int] = None
+    id: Optional[str] = None
     name: str 
-    address: str | None = None
+    address: Optional[str] = None
     cep: str
     cnpj: str
     phone_number: str 
     cuisine_type: str 
-    rating: Optional[float] 
+    rating: Optional[float] = None
     is_open: bool 
     occupancy: int 
     max_occupancy: int
