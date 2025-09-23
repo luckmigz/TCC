7e9deae7eafe1b1b9562232a5d1fc72749066081
Lucas M. <lucas.matosnegri@hotmail.com>
2025-09-23 20:42:15 -0300

    feat: adicionado arquivos para roda Heroku

--- backend/user_service/app.py
+++ backend/user_service/app.py
@@ -1,10 +1,6 @@
 from fastapi import FastAPI 
 from routes.users import router as user_router
-import uvicorn
 
 app = FastAPI()
 
 app.include_router(user_router, prefix="/user", tags=["Users"])
-
-if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0" , port=8001, reload=True)
