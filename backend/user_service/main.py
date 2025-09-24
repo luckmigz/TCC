1e80374c377d5588e1c294ee770dbde91e90e731
Lucas M. <lucas.matosnegri@hotmail.com>
2025-09-24 18:56:07 -0300

    Testando para rodar no heroku

--- backend/user_service/main.py
+++ backend/user_service/main.py
@@ -1,11 +1,8 @@
-import sys
-import os
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'app'))
 
 from fastapi import FastAPI 
 from app.routes.users import router as user_router
 
-app = FastAPI()
+main = FastAPI()
 
-app.include_router(user_router, prefix="/user", tags=["Users"])
+main.include_router(user_router, prefix="/user", tags=["Users"])
