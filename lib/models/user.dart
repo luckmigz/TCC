aa63f52c3029b599ca926afd487337cc8cc04c8a
Lucas M. <lucas.matosnegri@hotmail.com>
2025-10-21 10:48:56 -0300

    feat: adicionando rotas do back end

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,6 +1,23 @@
 class User {
   final String name;
   final String email;
+  final String password;
+  final String cpf;
 
-  User({required this.name, required this.email});
+  User({
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.cpf,
+  });
+
+  // --- ADICIONE ESTE MÉTODO ---
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'email': email,
+      'password': password,
+      'cpf': cpf,
+    };
+  }
 }
--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,3 +1,4 @@
+
 class User {
   final String name;
   final String email;
